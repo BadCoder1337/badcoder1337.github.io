3c6d525a5834f680d7741fc792e14b0a41f0c14a
BadCoder1337 <anton.tiz.musin@gmail.com>
2018-07-13 16:52:54 +0300

    add vote

--- hud/js/main.js
+++ hud/js/main.js
@@ -1,14 +1,8 @@
-let settings = JSON.parse(b64DecodeUnicode(getParameterByName('c')));
+const settings = JSON.parse(b64DecodeUnicode(getParameterByName('c')));
 
 for (let i = 0; i < 2; i++) {
     $('.team-title')[i].innerHTML = settings.title[i];
-}
-
-for (let i = 0; i < 2; i++) {
     $('.header-logo')[i].style['background-image'] = `url(${settings.logo[i]})`;
-}
-
-for (let i = 0; i < 2; i++) {
     $('.score')[i].innerHTML = settings.score[i];
 }
 
---
+++ vote/js/main.js
@@ -0,0 +1,23 @@
+const settings = JSON.parse(b64DecodeUnicode(getParameterByName('c')));
+
+for (let i = 0; i < 2; i++) {
+    $('.team-title')[i].innerHTML = settings.title[i];
+    $('.logo')[i].style['background-image'] = `url(${settings.logo[i]})`;
+}
+
+$('.footer-name').text(settings.name);
+
+if (getParameterByName('f')) {
+    $('.header').show();
+} else {
+    $('.header').fadeIn();
+}
+
+const socket = io('http://localhost:9876');
+
+socket.on('message', (message)=>{
+    console.log(message);
+    for (let i = 0; i < 2; i++) {
+        $('.percent')[i].innerHTML = message.percent[i];  
+    }
+})
