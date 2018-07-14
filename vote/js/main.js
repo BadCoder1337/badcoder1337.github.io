61b7ac3ccb654b4122fb13f56a2636930ccc4eae
BadCoder1337 <anton.tiz.musin@gmail.com>
2018-07-14 19:40:41 +0300

    up

--- vote/js/main.js
+++ vote/js/main.js
@@ -1,3 +1,19 @@
+function getParameterByName(name) {
+    let url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function b64DecodeUnicode (str) {
+    return decodeURIComponent(atob(str).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+}
+
 const settings = JSON.parse(b64DecodeUnicode(getParameterByName('c')));
 
 for (let i = 0; i < 2; i++) {
