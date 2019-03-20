f8ded01f6c0981cc80a06009155bc0612e43b306
BadCoder1337 <anton.tiz.musin@gmail.com>
2019-03-20 17:29:52 +0300

    deprecated

--- hud/js/main.js
+++ hud/js/main.js
@@ -1,32 +1,16 @@
-function getParameterByName(name) {
-    let url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
-
-function b64DecodeUnicode (str) {
-    return decodeURIComponent(atob(str).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-}
-
-const settings = JSON.parse(b64DecodeUnicode(getParameterByName('c')));
+const settings = new URLSearchParams(window.location.search);
 
 for (let i = 0; i < 2; i++) {
-    $('.team-title')[i].innerHTML = settings.title[i];
-    $('.header-logo')[i].style['background-image'] = `url(${settings.logo[i]})`;
-    $('.score')[i].innerHTML = settings.score[i];
+    $('.team-title')[i].innerHTML = settings.get('title').split(',')[i];
+    $('.header-logo')[i].style['background-image'] = `url(${settings.get('logo').split(',')[i]})`;
+    $('.score')[i].innerHTML = settings.get('score').split(',')[i];
 }
 
-$('.title').text(settings.name);
+$('.title').text(settings.get('name'));
 
 settings.esl ? $('.esl-logo').show() : $('.esl-logo').hide();
 
-if (getParameterByName('f')) {
+if (settings.get('f')) {
     $('.header').show();
 } else {
     $('.header').fadeIn();
--- vote/js/main.js
+++ vote/js/main.js
@@ -1,29 +1,13 @@
-function getParameterByName(name) {
-    let url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
-
-function b64DecodeUnicode (str) {
-    return decodeURIComponent(atob(str).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-}
-
-const settings = JSON.parse(b64DecodeUnicode(getParameterByName('c')));
+const settings = new URLSearchParams(window.location.search);
 
 for (let i = 0; i < 2; i++) {
-    $('.team-title')[i].innerHTML = settings.title[i];
-    $('.logo')[i].style['background-image'] = `url(${settings.logo[i]})`;
+    $('.team-title')[i].innerHTML = settings.get('title').split(',')[i]
+    $('.logo')[i].style['background-image'] = `url(${settings.get('logo').split(',')[i]})`;
 }
 
-$('.footer-name').text(settings.name);
+$('.footer-name').text(settings.get('name'));
 
-if (getParameterByName('f')) {
+if (settings.get('f')) {
     $('.footer').show();
 } else {
     $('.footer').fadeIn();
